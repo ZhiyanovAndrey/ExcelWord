0f29dccbe13f3d81b16ba513ef6cf801af010d93
Andrey <aazhiyanov@gmail.com>
2024-03-20 16:46:31 +0300

    add OpenExceleFile

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System.Windows;
+﻿using System.Data;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
 using ExcelDataReader;
+using Microsoft.Win32;
 namespace ExcelWord
 {
     /// <summary>
@@ -7,9 +11,53 @@ namespace ExcelWord
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string filename = string.Empty;
+
+
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            //try
+            //{
+            //    OpenFileDialog ofd = new OpenFileDialog
+            //    {
+            //        Filter = "";
+            //    };  
+
+            //}
+            //catch (Exception ex)
+            //{
+
+            //    MessageBox.Show(ex.Message);
+            //}
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenExceleFile(textbox.Text);
+        }
+
+        private void OpenExceleFile(string path)
+        {
+            FileStream fileStream=File.Open(path,FileMode.Open,FileAccess.Read);
+            IExcelDataReader reader = ExcelReaderFactory.CreateReader(fileStream);  // приводим поток к интерфейсу
+
+            DataSet db = reader.AsDataSet(new ExcelDataSetConfiguration() // создадим бд
+            {
+                ConfigureDataTable=(x) => new ExcelDataTableConfiguration()
+                {
+                    UseHeaderRow = true   // считываем верхнюю строку с названием колонок
+                }
+            });
+
+            // присвоим
+            datagrid1.ItemsSource=db.Tables;
+
         }
     }
 }
