1d08353f7c2a0d347e61611e2b0a2314ca4a8f21
Andrey <aazhiyanov@gmail.com>
2024-03-21 09:05:43 +0300

    add class ExcelData

---
+++ ExcelData.cs
@@ -0,0 +1,79 @@
+﻿using Aspose.Cells;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelWord
+{
+    public class ExcelData
+    {
+        // Табельный номер	Фамилия	Имя 	Отчество	Дата рождения	Отдел
+
+        public int Id { get; set; }
+        public string SurName { get; set; }
+        public string FirstName { get; set; }
+        public string? MiddleName { get; set; }
+        public DateTime Birthday { get; set; }
+        public int Department { get; set; }
+
+        public ExcelData() { }
+
+        private void OpenExceleFile(string path)
+        {
+            // Загрузить файл Excel
+            Workbook wb = new Workbook(path);
+
+            // Получить все рабочие листы
+            WorksheetCollection collection = wb.Worksheets;
+
+            // Перебрать все рабочие листы
+            for (int worksheetIndex = 0; worksheetIndex < collection.Count; worksheetIndex++)
+            {
+
+                // Получить рабочий лист, используя его индекс
+                Worksheet worksheet = collection[worksheetIndex];
+
+                // Печать имени рабочего листа
+                Console.WriteLine("Worksheet: " + worksheet.Name);
+
+                // Получить количество строк и столбцов
+                int rows = worksheet.Cells.MaxDataRow;
+                int cols = worksheet.Cells.MaxDataColumn;
+
+                // Цикл по строкам
+                for (int i = 0; i < rows; i++)
+                {
+
+                    // Перебрать каждый столбец в выбранной строке
+                    for (int j = 0; j < cols; j++)
+                    {
+                        // Значение ячейки Pring
+                        Console.Write(worksheet.Cells[i, j].Value + " | ");
+                    }
+                    // Распечатать разрыв строки
+                    Console.WriteLine(" ");
+                }
+            }
+
+
+
+
+
+
+            //FileStream fileStream=File.Open(path,FileMode.Open,FileAccess.Read);
+            //IExcelDataReader reader = ExcelReaderFactory.CreateReader(fileStream);  // приводим поток к интерфейсу
+
+            //DataSet db = reader.AsDataSet(new ExcelDataSetConfiguration() // создадим бд
+            //{
+            //    ConfigureDataTable=(x) => new ExcelDataTableConfiguration()
+            //    {
+            //        UseHeaderRow = true   // считываем верхнюю строку с названием колонок
+            //    }
+            //});
+
+            //// присвоим
+            //datagrid1.ItemsSource=db.Tables;
+        }
+    }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -40,64 +40,10 @@ namespace ExcelWord
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            OpenExceleFile(textbox.Text);
+            OpenExcelFile(@"D:\Data.xlsx");
         }
 
-        private void OpenExceleFile(string path)
-        {
-            // Загрузить файл Excel
-            Workbook wb = new Workbook("excel.xlsx");
 
-            // Получить все рабочие листы
-            WorksheetCollection collection = wb.Worksheets;
-
-            // Перебрать все рабочие листы
-            for (int worksheetIndex = 0; worksheetIndex < collection.Count; worksheetIndex++)
-            {
-
-                // Получить рабочий лист, используя его индекс
-                Worksheet worksheet = collection[worksheetIndex];
-
-                // Печать имени рабочего листа
-                Console.WriteLine("Worksheet: " + worksheet.Name);
-
-                // Получить количество строк и столбцов
-                int rows = worksheet.Cells.MaxDataRow;
-                int cols = worksheet.Cells.MaxDataColumn;
-
-                // Цикл по строкам
-                for (int i = 0; i < rows; i++)
-                {
-
-                    // Перебрать каждый столбец в выбранной строке
-                    for (int j = 0; j < cols; j++)
-                    {
-                        // Значение ячейки Pring
-                        Console.Write(worksheet.Cells[i, j].Value + " | ");
-                    }
-                    // Распечатать разрыв строки
-                    Console.WriteLine(" ");
-                }
-            }
-
-
-
-
-
-
-            //FileStream fileStream=File.Open(path,FileMode.Open,FileAccess.Read);
-            //IExcelDataReader reader = ExcelReaderFactory.CreateReader(fileStream);  // приводим поток к интерфейсу
-
-            //DataSet db = reader.AsDataSet(new ExcelDataSetConfiguration() // создадим бд
-            //{
-            //    ConfigureDataTable=(x) => new ExcelDataTableConfiguration()
-            //    {
-            //        UseHeaderRow = true   // считываем верхнюю строку с названием колонок
-            //    }
-            //});
-
-            //// присвоим
-            //datagrid1.ItemsSource=db.Tables;
 
         }
     }
